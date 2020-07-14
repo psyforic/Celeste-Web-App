625c9e2eb108bf32da6ec4f66370d3d167f2235e
Siphosenkosi <siphosenkosi@developmenthub.co.za>
2020-07-14 15:01:04 +0200

    Modes Screen

--- celesteangular/src/app/modes/modes.component.ts
+++ celesteangular/src/app/modes/modes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-modes',
@@ -8,13 +8,14 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class ModesComponent implements OnInit {
   closeResult = '';
+  currentOrientation = 'horizontal';
   constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
 
   open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
