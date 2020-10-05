b8926987f1507ae7109678d0e8898b4731942fbd
Siphosenkosi <siphosenkosi@developmenthub.co.za>
2020-10-05 04:45:19 +0200

    pull modes and tenants for host users

--- angular/src/host/modes/modes.component.ts
+++ angular/src/host/modes/modes.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { PagedListingComponentBase, PagedRequestDto } from '@shared/paged-listing-component-base';
+import { ModeListDto, ModeServiceProxy } from '@shared/service-proxies/service-proxies';
+import { finalize } from 'rxjs/operators';
+
+
+class PagedModesRequestDto extends PagedRequestDto {
+  keyword: string;
+  isActive: boolean | null;
+}
 
 @Component({
   selector: 'app-modes',
   templateUrl: './modes.component.html',
-  styleUrls: ['./modes.component.css']
+  styleUrls: ['./modes.component.css'],
+  providers: [ModeServiceProxy]
 })
-export class ModesComponent implements OnInit {
+export class ModesComponent extends PagedListingComponentBase<ModeListDto> {
 
-  constructor() { }
+  closeResult = '';
+  currentOrientation = 'horizontal';
+  isLoading = false;
+  isActive = false;
+  modes: ModeListDto[] = [];
+  keyword = '';
 
-  ngOnInit() {
+  constructor(
+    private _ModeService: ModeServiceProxy,
+    injector: Injector,
+  ) {
+    super(injector);
+  }
+  protected delete(entity: ModeListDto): void {
+    abp.message.confirm(
+      'You want to Delete ' + entity.name + '?', '',
+      (result: boolean) => {
+        if (result) {
+          this._ModeService.delete(entity.id)
+            .pipe(finalize(() => {
+              this.isLoading = false;
+            }))
+            .subscribe(() => {
+              abp.notify.success('Deleted Successfully');
+              this.refresh();
+            });
+        }
+      }
+    );
+  }
+  protected list(
+    request: PagedModesRequestDto,
+    pageNumber: number,
+    finishedCallback: Function
+  ): void {
+    this.isLoading = true;
+    request.keyword = this.keyword;
+    request.isActive = this.isActive;
+
+    this._ModeService.getAll(request.maxResultCount, request.skipCount)
+      .pipe(
+        finalize(() => {
+          finishedCallback();
+          this.isLoading = false;
+        })
+      )
+      .subscribe((result: any) => {
+        this.modes = result.items;
+        console.log(this.modes);
+        this.showPaging(result, pageNumber);
+      });
   }
 
 }
--- angular/src/host/tenants/Tenants.component.ts
+++ angular/src/host/tenants/Tenants.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { PagedListingComponentBase, PagedRequestDto } from '@shared/paged-listing-component-base';
+import { TenantDto, TenantDtoPagedResultDto, TenantServiceProxy } from '@shared/service-proxies/service-proxies';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { finalize } from 'rxjs/operators';
 
+class PagedTenantsRequestDto extends PagedRequestDto {
+  keyword: string;
+  isActive: boolean | null;
+}
 @Component({
   selector: 'host-tenants',
   templateUrl: './Tenants.component.html',
   styleUrls: ['./Tenants.component.css']
 })
-export class TenantsComponent implements OnInit {
+export class TenantsComponent extends PagedListingComponentBase<TenantDto> {
+  tenants: TenantDto[] = [];
+  keyword = '';
+  isActive: boolean | null;
+  advancedFiltersVisible = false;
+  constructor(
+    injector: Injector,
+    private _tenantService: TenantServiceProxy,
+    private _modalService: BsModalService
+  ) {
+    super(injector);
+  }
+  list(
+    request: PagedTenantsRequestDto,
+    pageNumber: number,
+    finishedCallback: Function
+  ): void {
+    request.keyword = this.keyword;
+    request.isActive = this.isActive;
 
-  constructor() { }
-
-  ngOnInit() {
+    this._tenantService
+      .getAll(
+        request.keyword,
+        request.isActive,
+        request.skipCount,
+        request.maxResultCount
+      )
+      .pipe(
+        finalize(() => {
+          finishedCallback();
+        })
+      )
+      .subscribe((result: TenantDtoPagedResultDto) => {
+        this.tenants = result.items;
+        this.showPaging(result, pageNumber);
+      });
+  }
+  protected delete(entity: TenantDto): void {
+    throw new Error('Method not implemented.');
   }
 
 }
